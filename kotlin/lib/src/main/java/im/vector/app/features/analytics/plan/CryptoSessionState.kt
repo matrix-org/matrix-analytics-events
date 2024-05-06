de5623e0f1932bac3e129c4f72a6e4ede1e8b305
Valere <bill.carson@valrsoft.com>
2024-05-06 12:39:56 +0200

    Add a new crypto session state event

---
+++ kotlin/lib/src/main/java/im/vector/app/features/analytics/plan/CryptoSessionState.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.analytics.plan
+
+import im.vector.app.features.analytics.itf.VectorAnalyticsEvent
+
+// GENERATED FILE, DO NOT EDIT. FOR MORE INFORMATION VISIT
+// https://github.com/matrix-org/matrix-analytics-events/
+
+/**
+ * Describe the current session crypto state, that is if the session is verified
+ * or not, if recovery is correctly setup.
+ */
+data class CryptoSessionState(
+        val recoveryState: RecoveryState,
+        val verificationState: VerificationState,
+) : VectorAnalyticsEvent {
+
+    enum class VerificationState {
+        /**
+         * The device is unverified.
+         */
+        NotVerified,
+
+        /**
+         * The state is unknown for now.
+         */
+        Unknown,
+
+        /**
+         * The device is considered to be verified, it has been signed by its
+         * user identity.
+         */
+        Verified,
+    }
+
+    enum class RecoveryState {
+
+        /**
+         * No default secret storage key exists or it is disabled explicitly
+         * using the account data event.
+         */
+        Disabled,
+
+        /**
+         * Secret storage is set up and we have all the secrets locally.
+         */
+        Enabled,
+
+        /**
+         * Secret storage is set up but we're missing some secrets.
+         */
+        Incomplete,
+
+        /**
+         * The state is unknown for now.
+         */
+        Unknown,
+    }
+
+    override fun getName() = "CryptoSessionState"
+
+    override fun getProperties(): Map<String, Any>? {
+        return mutableMapOf<String, Any>().apply {
+            put("recoveryState", recoveryState.name)
+            put("verificationState", verificationState.name)
+        }.takeIf { it.isNotEmpty() }
+    }
+}
---
+++ types/swift/CryptoSessionState.swift
@@ -0,0 +1,62 @@
+//
+// Copyright 2021 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+// GENERATED FILE, DO NOT EDIT. FOR MORE INFORMATION VISIT
+// https://github.com/matrix-org/matrix-analytics-events/
+
+/// Describe the current session crypto state, that is if the session is verified or not, if recovery is correctly setup.
+extension AnalyticsEvent {
+    public struct CryptoSessionState: AnalyticsEventProtocol {
+        public let eventName = "CryptoSessionState"
+
+        public let recoveryState: RecoveryState
+        public let verificationState: VerificationState
+
+        public init(recoveryState: RecoveryState, verificationState: VerificationState) {
+            self.recoveryState = recoveryState
+            self.verificationState = verificationState
+        }
+
+        public enum VerificationState: String {
+            /// The device is unverified.
+            case NotVerified
+            /// The state is unknown for now.
+            case Unknown
+            /// The device is considered to be verified, it has been signed by its user identity.
+            case Verified
+        }
+
+        public enum RecoveryState: String {
+            /// No default secret storage key exists or it is disabled explicitly using the account data event.
+            case Disabled
+            /// Secret storage is set up and we have all the secrets locally.
+            case Enabled
+            /// Secret storage is set up but we're missing some secrets.
+            case Incomplete
+            /// The state is unknown for now.
+            case Unknown
+        }
+
+        public var properties: [String: Any] {
+            return [
+                "recoveryState": recoveryState.rawValue,
+                "verificationState": verificationState.rawValue
+            ]
+        }
+    }
+}
